38c18ec9929ebc0ea05068f94db26864dc9c9fda
Sumit Garg <sumit.garg@oss.qualcomm.com>
2026-02-04 18:29:01 +0530

    FROMLIST: media: qcom: Switch to generic PAS TZ APIs
    Switch qcom media client drivers over to generic PAS TZ APIs. Generic PAS
    TZ service allows to support multiple TZ implementation backends like QTEE
    based SCM PAS service, OP-TEE based PAS service and any further future TZ
    backend service.
    
    Along with that pass proper PAS ID to set_remote_state API. As per testing
    the SCM backend just ignores it while OP-TEE makes use of it to for proper
    book keeping purpose.
    
    Signed-off-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
    Link: https://lore.kernel.org/all/20260427095603.1157963-5-sumit.garg@kernel.org/

--- drivers/media/platform/qcom/iris/iris_firmware.c
+++ drivers/media/platform/qcom/iris/iris_firmware.c
@@ -5,6 +5,7 @@
 
 #include <linux/device.h>
 #include <linux/firmware.h>
+#include <linux/firmware/qcom/qcom_pas.h>
 #include <linux/firmware/qcom/qcom_scm.h>
 #include <linux/iommu.h>
 #include <linux/of.h>
@@ -100,8 +101,8 @@ static const struct firmware *iris_detect_firmware(struct iris_core *core,
 
 static int iris_load_fw_to_memory(struct iris_core *core)
 {
+	struct qcom_pas_context *ctx;
 	const struct firmware *firmware = NULL;
-	struct qcom_scm_pas_context *ctx;
 	struct device *dev = core->dev;
 	struct resource res;
 	phys_addr_t mem_phys;
@@ -119,7 +120,7 @@ static int iris_load_fw_to_memory(struct iris_core *core)
 
 	dev = core->fw.dev ? : core->dev;
 
-	ctx = devm_qcom_scm_pas_context_alloc(dev, IRIS_PAS_ID, mem_phys, res_size);
+	ctx = devm_qcom_pas_context_alloc(dev, IRIS_PAS_ID, mem_phys, res_size);
 	if (!ctx)
 		return -ENOMEM;
 
@@ -138,7 +139,7 @@ static int iris_load_fw_to_memory(struct iris_core *core)
 	}
 
 	ret = qcom_mdt_pas_load(ctx, firmware, fw_name, NULL);
-	qcom_scm_pas_metadata_release(ctx);
+	qcom_pas_metadata_release(ctx);
 	if (ret)
 		goto err_release_fw;
 
@@ -149,7 +150,7 @@ static int iris_load_fw_to_memory(struct iris_core *core)
 			goto err_release_fw;
 	}
 
-	ret = qcom_scm_pas_prepare_and_auth_reset(ctx);
+	ret = qcom_pas_prepare_and_auth_reset(ctx);
 	if (ret)
 		goto err_iommu_unmap;
 
@@ -184,7 +185,7 @@ int iris_fw_load(struct iris_core *core)
 						     cp_config->cp_nonpixel_size);
 		if (ret) {
 			dev_err(core->dev, "qcom_scm_mem_protect_video_var failed: %d\n", ret);
-			qcom_scm_pas_shutdown(IRIS_PAS_ID);
+			qcom_pas_shutdown(IRIS_PAS_ID);
 			return ret;
 		}
 	}
@@ -194,13 +195,13 @@ int iris_fw_load(struct iris_core *core)
 
 int iris_fw_unload(struct iris_core *core)
 {
-	struct qcom_scm_pas_context *ctx = core->fw.ctx;
+	struct qcom_pas_context *ctx = core->fw.ctx;
 	int ret;
 
 	if (!ctx)
 		return -EINVAL;
 
-	ret = qcom_scm_pas_shutdown(ctx->pas_id);
+	ret = qcom_pas_shutdown(ctx->pas_id);
 	if (core->fw.iommu_domain)
 		iommu_unmap(core->fw.iommu_domain, 0, ctx->mem_size);
 
@@ -210,7 +211,7 @@ int iris_fw_unload(struct iris_core *core)
 
 int iris_set_hw_state(struct iris_core *core, bool resume)
 {
-	return qcom_scm_set_remote_state(resume, 0);
+	return qcom_pas_set_remote_state(resume, IRIS_PAS_ID);
 }
 
 int iris_fw_init(struct iris_core *core)
