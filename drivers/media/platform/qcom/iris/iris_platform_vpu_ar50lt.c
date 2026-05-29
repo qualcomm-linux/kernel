2686ef7939b6774cea34b6fa69a20faa36135a37
Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
2026-05-29 17:26:11 +0300

    BACKPORT: media: iris: drop struct iris_fmt
    The struct iris_fmt unites pixfmt with the plane type, however the type
    from the struct is not actually used. Drop the struct completely and use
    u32 pixfmt in all the callsites.
    
    Link: https://lore.kernel.org/all/20260529-iris-remote-fmts-v7-1-a8bd57ac8b5a@oss.qualcomm.com/
    Reviewed-by: Dikshita Agarwal <dikshita.agarwal@oss.qualcomm.com>
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Signed-off-by: Bryan O'Donoghue <bod@kernel.org>

--- drivers/media/platform/qcom/iris/iris_platform_vpu_ar50lt.c
+++ drivers/media/platform/qcom/iris/iris_platform_vpu_ar50lt.c
@@ -25,19 +25,10 @@ const struct iris_firmware_desc iris_vpu_ar50lt_p1_gen2_s6_desc = {
 	.fwname = "qcom/vpu/ar50lt_p1_gen2_s6.mbn",
 };
 
-static struct iris_fmt iris_fmts_ar50lt_dec[] = {
-	[IRIS_FMT_H264] = {
-		.pixfmt = V4L2_PIX_FMT_H264,
-		.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE,
-	},
-	[IRIS_FMT_HEVC] = {
-		.pixfmt = V4L2_PIX_FMT_HEVC,
-		.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE,
-	},
-	[IRIS_FMT_VP9] = {
-		.pixfmt = V4L2_PIX_FMT_VP9,
-		.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE,
-	},
+static const u32 iris_fmts_ar50lt_dec[] = {
+	[IRIS_FMT_H264] = V4L2_PIX_FMT_H264,
+	[IRIS_FMT_HEVC] = V4L2_PIX_FMT_HEVC,
+	[IRIS_FMT_VP9] = V4L2_PIX_FMT_VP9,
 };
 
 static const struct bw_info iris_bw_table_dec_ar50lt[] = {
