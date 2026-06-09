4a47a1eab8b87120df07897bcfe27633630eb335
David Heidelberg <david@ixit.cz>
2026-06-09 14:47:14 -0700

    FROMLIST: media: qcom: camss: csiphy-3ph: Use odd bits for configuring C-PHY lanes
    So far, only D-PHY mode was supported, which uses even bits when enabling
    or masking lanes. For C-PHY configuration, the hardware instead requires
    using the odd bits.
    
    Since there can be unrecognized configuration allow returning failure.
    
    Link: https://lore.kernel.org/r/20260301-qcom-cphy-v4-2-e53316d2cc65@ixit.cz/
    
    Signed-off-by: David Heidelberg <david@ixit.cz>
    Signed-off-by: Anusha Arun Nandi <anusha.nandi@oss.qualcomm.com>

--- drivers/media/platform/qcom/camss/camss-csiphy.c
+++ drivers/media/platform/qcom/camss/camss-csiphy.c
@@ -267,6 +267,7 @@ static int csiphy_set_power_legacy(struct v4l2_subdev *sd, int on)
 static int csiphy_stream_on_legacy(struct csiphy_device *csiphy)
 {
 	struct csiphy_config *cfg = &csiphy->cfg;
+	const struct csiphy_hw_ops *ops = csiphy->res->hw_ops;
 	s64 link_freq;
 	u8 lane_mask = csiphy->res->hw_ops->get_lane_mask(&cfg->csi2->lane_cfg);
 	u8 bpp = csiphy_get_bpp(csiphy->res->formats->formats, csiphy->res->formats->nformats,
@@ -297,9 +298,7 @@ static int csiphy_stream_on_legacy(struct csiphy_device *csiphy)
 		wmb();
 	}
 
-	csiphy->res->hw_ops->lanes_enable(csiphy, cfg, link_freq, lane_mask);
-
-	return 0;
+	return ops->lanes_enable(csiphy, cfg, link_freq, lane_mask);
 }
 
 /*
