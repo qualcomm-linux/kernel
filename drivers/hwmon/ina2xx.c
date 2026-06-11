e95d9a5dd98212b4059ba9901c0bbf0f572b7654
Martino Facchin <m.facchin@arduino.cc>
2026-06-11 16:05:25 +0200

    BACKPORT: hwmon: (ina2xx) Add support for INA232
    The INA232 is a current/power monitor. It shares the same register
    layout as the other chips in the series and uses the INA226 default
    configuration, but differs in its electrical characteristics:
    
    Signed-off-by: Martino Facchin <m.facchin@arduino.cc>
    Signed-off-by: Loic Poulain <loic.poulain@oss.qualcomm.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260611-monza-ina232-v2-2-e4375ce652d0@oss.qualcomm.com
    [groeck: Rephrased commit message]
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    (cherry picked from commit d1db9b08fbf399ace18cb99adfd609d7e5582b73)
    [rsalveti: keep the positional i2c_device_id initialisers this branch uses;
     the conversion to designated initialisers is not part of this series]
    Signed-off-by: Ricardo Salveti <ricardo.salveti@oss.qualcomm.com>

--- drivers/hwmon/ina2xx.c
+++ drivers/hwmon/ina2xx.c
@@ -138,6 +138,7 @@ static const struct regmap_config ina2xx_regmap_config = {
 enum ina2xx_ids {
 	ina219,
 	ina226,
+	ina232,
 	ina234,
 	ina260,
 	sy24655
@@ -211,6 +212,20 @@ static const struct ina2xx_config ina2xx_config[] = {
 		.has_ishunt = false,
 		.has_power_average = false,
 		.current_shift = 4,
+		.has_update_interval = true,
+	},
+	[ina232] = {
+		.config_default = INA226_CONFIG_DEFAULT,
+		.calibration_value = 2048,
+		.shunt_div = 400,
+		.shunt_voltage_shift = 0,
+		.bus_voltage_shift = 0,
+		.bus_voltage_lsb = 1600,
+		.power_lsb_factor = 32,
+		.has_alerts = true,
+		.has_ishunt = false,
+		.has_power_average = false,
+		.current_shift = 0,
 		.has_update_interval = true,
 	},
 	[ina260] = {
@@ -1029,6 +1044,7 @@ static const struct i2c_device_id ina2xx_id[] = {
 	{ "ina226", ina226 },
 	{ "ina230", ina226 },
 	{ "ina231", ina226 },
+	{ "ina232", ina232 },
 	{ "ina234", ina234 },
 	{ "ina260", ina260 },
 	{ "sy24655", sy24655 },
@@ -1060,6 +1076,10 @@ static const struct of_device_id __maybe_unused ina2xx_of_match[] = {
 	{
 		.compatible = "ti,ina231",
 		.data = (void *)ina226
+	},
+	{
+		.compatible = "ti,ina232",
+		.data = (void *)ina232
 	},
 	{
 		.compatible = "ti,ina234",
