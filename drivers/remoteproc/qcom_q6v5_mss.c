effaa89362ffb27cc65e2f767392cc08f378af59
Sumit Garg <sumit.garg@oss.qualcomm.com>
2026-07-02 17:28:21 +0530

    BACKPORT: remoteproc: qcom_q6v5_mss: Switch to generic PAS TZ APIs
    Switch qcom_q6v5_mss client driver over to generic PAS TZ APIs. Generic PAS
    TZ service allows to support multiple TZ implementation backends like QTEE
    based SCM PAS service, OP-TEE based PAS service and any further future TZ
    backend service.
    
    Reviewed-by: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
    Tested-by: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com> # Lemans
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Signed-off-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260702115835.167602-6-sumit.garg@kernel.org
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    (cherry picked from commit f3b1357673ddb37ae8b9a8fe44df73cbd2a519c5)
    [rsalveti: keep this branch's "qproc->version == MSS_MSM8953" condition, as f56052f4d87c ("remoteproc: qcom_q6v5_mss: Introduce need_pas_mem_setup") is not part of it]
    Signed-off-by: Ricardo Salveti <ricardo.salveti@oss.qualcomm.com>

--- drivers/remoteproc/qcom_q6v5_mss.c
+++ drivers/remoteproc/qcom_q6v5_mss.c
@@ -34,6 +34,7 @@
 #include "qcom_pil_info.h"
 #include "qcom_q6v5.h"
 
+#include <linux/firmware/qcom/qcom_pas.h>
 #include <linux/firmware/qcom/qcom_scm.h>
 
 #define MPSS_CRASH_REASON_SMEM		421
@@ -1441,7 +1442,7 @@ static int q6v5_mpss_load(struct q6v5 *qproc)
 	}
 
 	if (qproc->version == MSS_MSM8953) {
-		ret = qcom_scm_pas_mem_setup(MPSS_PAS_ID, qproc->mpss_phys, qproc->mpss_size);
+		ret = qcom_pas_mem_setup(MPSS_PAS_ID, qproc->mpss_phys, qproc->mpss_size);
 		if (ret) {
 			dev_err(qproc->dev,
 				"setting up mpss memory failed: %d\n", ret);
@@ -2059,7 +2060,7 @@ static int q6v5_probe(struct platform_device *pdev)
 	if (!desc)
 		return -EINVAL;
 
-	if (desc->need_mem_protection && !qcom_scm_is_available())
+	if (desc->need_mem_protection && !qcom_pas_is_available())
 		return -EPROBE_DEFER;
 
 	mba_image = desc->hexagon_mba_image;
