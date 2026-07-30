d8058860fe22432689e5d8a28f0c8c2058e6af5a
Richard Acayan <mailingradian@gmail.com>
2026-07-30 13:43:44 -0400

    FROMGIT: ASoC: qdsp6: q6dsp-lpass-ports: add support for lpi mi2s ports 5-6
    Add the extra LPI MI2S ports used for internal MI2S on SDM660.
    
    Link: https://android.googlesource.com/kernel/msm-extra/+/530cffa4cc977a348753831b163eb9d3302b954a/asoc/msm-dai-q6-v2.c#4597
    Signed-off-by: Richard Acayan <mailingradian@gmail.com>
    Reviewed-by: Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260730174353.108023-7-mailingradian@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 68400191e005f44c432c1b18eeca7f5527323a9f)
    
    Signed-off-by: Ricardo Salveti <ricardo.salveti@oss.qualcomm.com>

--- sound/soc/qcom/common.h
+++ sound/soc/qcom/common.h
@@ -7,7 +7,7 @@
 #include <dt-bindings/sound/qcom,q6afe.h>
 #include <sound/soc.h>
 
-#define LPASS_MAX_PORT			(SENARY_MI2S_TX + 1)
+#define LPASS_MAX_PORT			(LPI_MI2S_TX_6 + 1)
 
 struct qcom_snd_tdm_slot_cfg {
 	unsigned int tx_mask;
