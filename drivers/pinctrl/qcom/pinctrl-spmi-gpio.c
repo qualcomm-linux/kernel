f42599c782033b7ae995a8ba52413225f660b22a
Dhruvin Rajpura <drajpura@qti.qualcomm.com>
2026-08-14 14:08:12 +0530

    pinctrl: qcom: spmi-gpio: add PMAU0102 and PMM8650AU compatibles
    Add SPMI GPIO compatible strings for the PMAU0102 and PMM8650AU PMICs
    used on Nord, each with 12 GPIOs, so the driver can bind against the
    new per-chip PMIC dtsi nodes.
    
    Signed-off-by: Dhruvin Rajpura <drajpura@qti.qualcomm.com>

--- drivers/pinctrl/qcom/pinctrl-spmi-gpio.c
+++ drivers/pinctrl/qcom/pinctrl-spmi-gpio.c
@@ -1274,7 +1274,9 @@ static const struct of_device_id pmic_gpio_of_match[] = {
 	{ .compatible = "qcom,pmk8350-gpio", .data = (void *) 4 },
 	{ .compatible = "qcom,pmk8550-gpio", .data = (void *) 6 },
 	{ .compatible = "qcom,pmk8850-gpio", .data = (void *)8 },
+	{ .compatible = "qcom,pmau0102-gpio", .data = (void *) 12 },
 	{ .compatible = "qcom,pmm8155au-gpio", .data = (void *) 10 },
+	{ .compatible = "qcom,pmm8650au-gpio", .data = (void *) 12 },
 	{ .compatible = "qcom,pmm8654au-gpio", .data = (void *) 12 },
 	/* pmp8074 has 12 GPIOs with holes on 1 and 12 */
 	{ .compatible = "qcom,pmp8074-gpio", .data = (void *) 12 },
