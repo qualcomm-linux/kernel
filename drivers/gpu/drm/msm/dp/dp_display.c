a601e2cdea53c6c3ff561a31046e67461b6b0724
Yongxing Mou <yongxing.mou@oss.qualcomm.com>
2026-09-07 12:37:41 +0800

    Revert "FROMLIST: drm/msm/dp: Add support for programming p1/p2/p3 register blocks"
    This reverts commit c8a98357d82dfbc75289d200a530b43068ab18e1.
    
    This is reverted because a newer version of this patch is being introduced.
    
    Signed-off-by: Yongxing Mou <yongxing.mou@oss.qualcomm.com>

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -87,8 +87,8 @@ struct msm_dp_display_private {
 	void __iomem *link_base;
 	size_t link_len;
 
-	void __iomem *pixel_base[DP_STREAM_MAX];
-	size_t pixel_len;
+	void __iomem *p0_base;
+	size_t p0_len;
 
 	int max_stream;
 };
@@ -595,7 +595,7 @@ struct msm_dp_panel *msm_dp_display_get_panel(struct msm_dp *msm_dp_display)
 	dp = container_of(msm_dp_display, struct msm_dp_display_private, msm_dp_display);
 
 	dp_panel = msm_dp_panel_get(&dp->msm_dp_display.pdev->dev, dp->aux, dp->link,
-				    dp->link_base, dp->pixel_base[0]);
+				    dp->link_base, dp->p0_base);
 
 	if (IS_ERR(dp->panel)) {
 		DRM_ERROR("failed to initialize panel\n");
@@ -649,7 +649,7 @@ static int msm_dp_init_sub_modules(struct msm_dp_display_private *dp)
 		goto error_link;
 	}
 
-	dp->panel = msm_dp_panel_get(dev, dp->aux, dp->link, dp->link_base, dp->pixel_base[0]);
+	dp->panel = msm_dp_panel_get(dev, dp->aux, dp->link, dp->link_base, dp->p0_base);
 	if (IS_ERR(dp->panel)) {
 		rc = PTR_ERR(dp->panel);
 		DRM_ERROR("failed to initialize panel, rc = %d\n", rc);
@@ -853,7 +853,7 @@ int msm_dp_display_set_stream_info(struct msm_dp *msm_dp_display, struct msm_dp_
 	if (panel) {
 		panel->stream_id = stream_id;
 		panel->pbn = pbn;
-		msm_dp_panel_set_pixel_base(panel, dp->pixel_base[stream_id]);
+		msm_dp_panel_set_pixel_base(panel, dp->p0_base);
 	}
 
 	return rc;
@@ -968,14 +968,8 @@ void msm_dp_snapshot(struct msm_disp_state *disp_state, struct msm_dp *dp)
 				    msm_dp_display->aux_base, "dp_aux");
 	msm_disp_snapshot_add_block(disp_state, msm_dp_display->link_len,
 				    msm_dp_display->link_base, "dp_link");
-	msm_disp_snapshot_add_block(disp_state, msm_dp_display->pixel_len,
-				    msm_dp_display->pixel_base[0], "dp_p0");
-	msm_disp_snapshot_add_block(disp_state, msm_dp_display->pixel_len,
-				    msm_dp_display->pixel_base[1], "dp_p1");
-	msm_disp_snapshot_add_block(disp_state, msm_dp_display->pixel_len,
-				    msm_dp_display->pixel_base[2], "dp_p2");
-	msm_disp_snapshot_add_block(disp_state, msm_dp_display->pixel_len,
-				    msm_dp_display->pixel_base[3], "dp_p3");
+	msm_disp_snapshot_add_block(disp_state, msm_dp_display->p0_len,
+				    msm_dp_display->p0_base, "dp_p0");
 }
 
 void msm_dp_display_set_psr(struct msm_dp *msm_dp_display, bool enter)
@@ -1264,7 +1258,6 @@ static void __iomem *msm_dp_ioremap(struct platform_device *pdev, int idx, size_
 static int msm_dp_display_get_io(struct msm_dp_display_private *display)
 {
 	struct platform_device *pdev = display->msm_dp_display.pdev;
-	int i;
 
 	display->ahb_base = msm_dp_ioremap(pdev, 0, &display->ahb_len);
 	if (IS_ERR(display->ahb_base))
@@ -1294,8 +1287,8 @@ static int msm_dp_display_get_io(struct msm_dp_display_private *display)
 		display->aux_len = DP_DEFAULT_AUX_SIZE;
 		display->link_base = display->ahb_base + DP_DEFAULT_LINK_OFFSET;
 		display->link_len = DP_DEFAULT_LINK_SIZE;
-		display->pixel_base[0] = display->ahb_base + DP_DEFAULT_P0_OFFSET;
-		display->pixel_len = DP_DEFAULT_P0_SIZE;
+		display->p0_base = display->ahb_base + DP_DEFAULT_P0_OFFSET;
+		display->p0_len = DP_DEFAULT_P0_SIZE;
 
 		return 0;
 	}
@@ -1306,20 +1299,10 @@ static int msm_dp_display_get_io(struct msm_dp_display_private *display)
 		return PTR_ERR(display->link_base);
 	}
 
-	display->pixel_base[0] = msm_dp_ioremap(pdev, 3, &display->pixel_len);
-	if (IS_ERR(display->pixel_base[0])) {
-		DRM_ERROR("unable to remap p0 region: %pe\n", display->pixel_base[0]);
-		return PTR_ERR(display->pixel_base[0]);
-	}
-
-	for (i = DP_STREAM_1; i < display->max_stream; i++) {
-		/* pixels clk reg index start from 3*/
-		display->pixel_base[i] = msm_dp_ioremap(pdev, i + 3, &display->pixel_len);
-		if (IS_ERR(display->pixel_base[i])) {
-			DRM_DEBUG_DP("unable to remap p%d region: %pe\n", i,
-				     display->pixel_base[i]);
-			break;
-		}
+	display->p0_base = msm_dp_ioremap(pdev, 3, &display->p0_len);
+	if (IS_ERR(display->p0_base)) {
+		DRM_ERROR("unable to remap p0 region: %pe\n", display->p0_base);
+		return PTR_ERR(display->p0_base);
 	}
 
 	return 0;
