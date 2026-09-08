411e087763b9d4cdc63c0fa2fec1a38016c7a849
Mohammad Rafi Shaik <mohammad.rafi.shaik@oss.qualcomm.com>
2026-09-08 15:26:23 +0530

    Revert "FROMLIST: ASoC: qcom: Add generic of_xlate_dai_name helper to common"
    This reverts commit f079100382f14ddcbe8c616538f630770c57f001.
    
    Signed-off-by: Mohammad Rafi Shaik <mohammad.rafi.shaik@oss.qualcomm.com>

--- sound/soc/qcom/common.h
+++ sound/soc/qcom/common.h
@@ -28,9 +28,4 @@ int qcom_snd_wcd_jack_setup(struct snd_soc_pcm_runtime *rtd,
 			    struct snd_soc_jack *jack, bool *jack_setup);
 int qcom_snd_dp_jack_setup(struct snd_soc_pcm_runtime *rtd,
 			   struct snd_soc_jack *dp_jack, int id);
-
-int asoc_qcom_of_xlate_dai_name(const struct snd_soc_dai_driver *dai_drv,
-				int num_dai,
-				const struct of_phandle_args *args,
-				const char **dai_name);
 #endif
